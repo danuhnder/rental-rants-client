8266795b917bb58acc644ed8b0c9d5147db27d11
Daniel Robertson <danieljrobertson@gmail.com>
2021-01-28 03:12:01 +0000

    make magic happen?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios'
+
 import reportWebVitals from './reportWebVitals';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(
   <React.StrictMode>
